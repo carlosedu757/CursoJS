7d8d67edde38362f5d7dda0a2fa0babd23a6de26
Carlos Eduardo <cerpcaca2013@gmail.com>
2023-02-06 19:45:46 -0300

    #Operadores de comparação

---
+++ aula21/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual
+< menor que
+<= menor que ou igual
+== igualdade (valor) ***********
+=== igualdade restrita (Valor e tipo)
+!= diferente (valor)
+!== diferente restrito (valor e tipo)
+*/
+const comp = 10 !== "10";
+console.log(comp);
